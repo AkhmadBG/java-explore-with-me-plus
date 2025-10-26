154e05757cdf7db93de3f336132fb02d93d66699
Maksim Torzhkov <maxtorj@yandex.ru>
2025-10-26 16:00:46 +0300

    feat: add event service interface regarding swagger demands

--- ewm-main/src/main/java/ru/practicum/ewm/main/service/event/EventService.java
+++ ewm-main/src/main/java/ru/practicum/ewm/main/service/event/EventService.java
@@ -1,4 +1,35 @@
 package ru.practicum.ewm.main.service.event;
 
-public class EventService {
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import ru.practicum.ewm.main.dto.event.*;
+import ru.practicum.ewm.main.entity.Event;
+import ru.practicum.ewm.main.enums.EventState;
+import ru.practicum.ewm.main.enums.SortValue;
+
+import java.util.List;
+
+public interface EventService {
+
+    EventFullDto createEvent(Long userId, NewEventDto newEventDto);
+
+    Page<EventShortDto> getEvents(Long userId, Pageable pageable);
+
+    EventFullDto updateEventByUser(Long userId, Long eventId, UpdateEventUserDto updateEventUserDto);
+
+    EventFullDto getEventByUser(Long userId, Long eventId);
+
+    EventFullDto updateEvent(Long eventId, UpdateEventAdminDto updateEventAdminDto);
+
+    EventFullDto getEvent(Long id, HttpServletRequest request);
+
+    void setView(List<Event> events);
+
+    List<EventFullDto> getEventsWithParamsByAdmin(List<Long> users, EventState states, List<Long> categoriesId,
+                                                  String rangeStart, String rangeEnd, Integer from, Integer size);
+
+    List<EventFullDto> getEventsWithParamsByUser(String text, List<Long> categories, Boolean paid, String rangeStart,
+                                                 String rangeEnd, Boolean onlyAvailable, SortValue sort, Integer from,
+                                                 Integer size, HttpServletRequest request);
 }
