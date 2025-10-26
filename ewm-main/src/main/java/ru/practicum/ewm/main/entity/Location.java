164474f813ad9bd29a197f1317dca5164c08738e
Maksim Torzhkov <maxtorj@yandex.ru>
2025-10-26 12:34:52 +0300

    feat: update Location entity

--- ewm-main/src/main/java/ru/practicum/ewm/main/entity/Location.java
+++ ewm-main/src/main/java/ru/practicum/ewm/main/entity/Location.java
@@ -1,4 +1,21 @@
 package ru.practicum.ewm.main.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "locations")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Float lat;
+    private Float lon;
 }
