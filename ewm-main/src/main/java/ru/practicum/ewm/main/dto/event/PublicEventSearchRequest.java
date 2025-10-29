84f44edc4d658087b397d932f9d1e6441e94bf05
Maksim Torzhkov <maxtorj@yandex.ru>
2025-10-29 11:26:40 +0300

    feat: add public event controller

--- ewm-main/src/main/java/ru/practicum/ewm/main/dto/event/PublicEventSearchRequest.java
+++ ewm-main/src/main/java/ru/practicum/ewm/main/dto/event/PublicEventSearchRequest.java
@@ -22,4 +22,28 @@ public class PublicEventSearchRequest {
     private SortValue sort;
     private Integer from;
     private Integer size;
+
+    public static PublicEventSearchRequest fromParams(
+            String text,
+            List<Long> categories,
+            Boolean paid,
+            String rangeStart,
+            String rangeEnd,
+            Boolean onlyAvailable,
+            SortValue sort,
+            Integer from,
+            Integer size) {
+
+        return PublicEventSearchRequest.builder()
+                .text(text)
+                .categories(categories)
+                .paid(paid)
+                .rangeStart(rangeStart)
+                .rangeEnd(rangeEnd)
+                .onlyAvailable(onlyAvailable != null ? onlyAvailable : false)
+                .sort(sort)
+                .from(from != null ? from : 0)
+                .size(size != null ? size : 10)
+                .build();
+    }
 }
